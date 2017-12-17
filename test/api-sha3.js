f5e86397751d0aedd6f47aca98f8a2ff68e58a46
Ryan Faulhaber <faulhaberryan@gmail.com>
2017-12-17 16:21:50 -0500

    removed stream

--- test/api-sha3.js
+++ test/api-sha3.js
@@ -2,35 +2,7 @@
 const Buffer = require('safe-buffer').Buffer
 const test = require('tape')
 
-const utf8text = 'УТФ-8 text'
-
 module.exports = (name, createHash) => {
-  test(`${name} Keccak#_transform`, (t) => {
-    t.test('should use Keccak#update', (t) => {
-      const hash = createHash('sha3-256')
-
-      t.plan(3)
-      hash.update = function () {
-        t.same(arguments[0], utf8text)
-        t.same(arguments[1], 'utf8')
-      }
-      hash._transform(utf8text, 'utf8', (err) => t.same(err, null))
-      t.end()
-    })
-
-    t.test('should handle error in Keccak#update', (t) => {
-      const hash = createHash('sha3-256')
-      const err = new Error('42')
-
-      t.plan(1)
-      hash.update = () => { throw err }
-      hash._transform(Buffer.alloc(0), 'buffer', (_err) => t.true(_err === err))
-      t.end()
-    })
-
-    t.end()
-  })
-
   test(`${name} Keccak#_flush`, (t) => {
     t.test('should use Keccak#digest', (t) => {
       const hash = createHash('sha3-256')
